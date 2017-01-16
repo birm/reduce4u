dadc6f2bc6e537c07d93f528f49320f649f46b74
Ryan Birmingham <birm@rbirm.us>
2017-01-16 11:35:43 -0500

    loader beta done

--- betas/loader.js
+++ betas/loader.js
@@ -1,3 +1,13 @@
+data_clean = function(data){
+  // convert to array, 1d
+  data = data.replace(/\n/g,',')
+  data = data.split(","); // TODO add other delimiters?
+  // map to float
+  data = data.map(parseFloat);
+  // remove nan
+  return data.filter(function(v) { return !!v; });
+}
+
 window.onload = function() {
     var fileInput = document.getElementById('fileInput');
     var fileDisplayArea = document.getElementById('fileDisplayArea');
@@ -13,20 +23,18 @@ window.onload = function() {
             reader.onload = function(e) {
                 // catch erorrs in matrix creation
                 try {
-                    var data = reader.result;
-                    // convert to array, 1d
-                    data = data.replace(/\n/g,',')
-                    data = data.split(","); // TODO add other delimiters?
-                    // map to float
-                    data = data.map(parseFloat);
+                    var data = data_clean(reader.result);
                     // get x and y sizes
-                    var x_len;
-                    var y_len;
+                    var x_len = document.getElementById('x_len');
+                    var y_len = document.getElementById('y_len');
+                    // make sure x and y are ints
+                    x_len = parseInt(x_len.value);
+                    y_len = parseInt(y_len.value);
                     // create matrix
-                    datamat = new Minimat(data, x_len, y_len);
+                    datamat = new MiniMat(data, x_len, y_len);
                     // display matrix out
-                    // fileDisplayArea.innerText = datamat.toString();
-                    fileDisplayArea.innerText = data;
+                     fileDisplayArea.innerText = datamat.toString();
+                    //fileDisplayArea.innerText = data;
                 }
                 catch (err) {
                     // print erorrs in matrix creation
