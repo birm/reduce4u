9f56b247f326d72d193bf16fe4ae6ae11fcec34e
Ryan Birmingham <birm@rbirm.us>
2017-01-16 16:36:09 -0500

    added vis notes

--- betas/visualize/visualize.js
+++ betas/visualize/visualize.js
@@ -1,1 +1,7 @@
 // TODO
+// start vaguely with http://bl.ocks.org/oyyd/859fafc8122977a3afd6
+
+// given a dimred object...
+// show initial on stage
+// slider for paramater
+// update stage with result of paramater
