8a2f3b4f0ab7b3b957ae8dd0b72068f2fc3b3d56
Ryan Birmingham <birm@rbirm.us>
2017-01-16 11:19:04 -0500

    added more loader code

--- betas/loader.js
+++ betas/loader.js
@@ -1,22 +1,43 @@
 window.onload = function() {
-        var fileInput = document.getElementById('fileInput');
-        var fileDisplayArea = document.getElementById('fileDisplayArea');
+    var fileInput = document.getElementById('fileInput');
+    var fileDisplayArea = document.getElementById('fileDisplayArea');
 
-        fileInput.addEventListener('change', function(e) {
-            var file = fileInput.files[0];
-            var textType = /text.*/;
+    fileInput.addEventListener('change', function(e) {
+        var file = fileInput.files[0];
+        var textType = /text.*/;
 
-            if (file.type.match(textType)) {
-                var reader = new FileReader();
+        if (file.type.match(textType)) {
+            var reader = new FileReader();
 
-                reader.onload = function(e) {
-                    fileDisplayArea.innerText = reader.result;
+            var datamat;
+            reader.onload = function(e) {
+                // catch erorrs in matrix creation
+                try {
+                    var data = reader.result;
+                    // convert to array, 1d
+                    data = data.replace(/\n/g,',')
+                    data = data.split(","); // TODO add other delimiters?
+                    // map to float
+                    data = data.map(parseFloat);
+                    // get x and y sizes
+                    var x_len;
+                    var y_len;
+                    // create matrix
+                    datamat = new Minimat(data, x_len, y_len);
+                    // display matrix out
+                    // fileDisplayArea.innerText = datamat.toString();
+                    fileDisplayArea.innerText = data;
                 }
-
-                reader.readAsText(file);
-            } else {
-                fileDisplayArea.innerText = "File not supported!"
+                catch (err) {
+                    // print erorrs in matrix creation
+                    fileDisplayArea.innerText = "ERROR LOADING: " + err.message;
+                }
             }
-        });
+
+            reader.readAsText(file);
+        } else {
+            fileDisplayArea.innerText = "File not supported!"
+        }
+    });
 }
 // this version from http://stackoverflow.com/questions/27522979/read-a-local-text-file-using-javascript
